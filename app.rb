72336e1ff120ac9d1058eed2edbccf333ac67f0d
Sachin Mahajan <sachinm78@gmail.com>
2019-04-17 20:04:28 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Sachin"
+  end
+
+  get '/hometown' do
+    "My hometown is Chicago"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Bohemian Rhapsody"
+  end
 end
